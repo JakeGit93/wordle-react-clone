90a8c656381bfbb3a30c8f4b92087034c18fa3a6
jakegit93 <jakejohnson1993@hotmail.com>
2024-09-19 20:28:40 +1000

    Added a helper functions file for handling user word guess attempts etc.

---
+++ src/functions.tsx
@@ -0,0 +1,7 @@
+import wordList from './assets/wordle.json'
+
+//TODO
+
+//add function for parsing guess attempts and then loading them into the letter tiles
+//add function for displaying correct, incorrect and partial letter guesses
+//add function for managing attempt number/remaining attemps
