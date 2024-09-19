98fb057e8fadc93c84d40f58b20e76f3b96cde8e
jakegit93 <jakejohnson1993@hotmail.com>
2024-09-19 20:49:43 +1000

    Updated TODO for functions.tsx

--- src/functions.tsx
+++ src/functions.tsx
@@ -4,4 +4,5 @@ import wordList from './assets/wordle.json'
 
 //add function for parsing guess attempts and then loading them into the letter tiles
 //add function for displaying correct, incorrect and partial letter guesses
-//add function for managing attempt number/remaining attemps
+//add function for managing attempt number/remaining attempts
+
