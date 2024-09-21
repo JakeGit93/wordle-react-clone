30f2e2cb76b3fabbe8d707152699eedf2d389ffd
jakegit93 <jakejohnsonbusiness@gmail.com>
2024-09-21 13:02:06 +1000

    Wrote generic templates for assumed necessary functions, unfinished

--- src/functions.tsx
+++ src/functions.tsx
@@ -5,13 +5,31 @@ import wordList from './assets/wordle.json'
 //add function for parsing guess attempts and then loading them into the letter tiles
 //add function for displaying correct, incorrect and partial letter guesses
 //add function for managing attempt number/remaining attempts
-//move GenerateWord component logic into a function in this file
 
 export function GenerateWord(){
     const randomValue = Math.floor(Math.random() * 2315);
 
     return(
         wordList[randomValue]
+    )
+}
+
+export function ParseGuess(){
+    const userInput = [];
+    return(
+        null
+    )
+}
+
+export function CheckGuess(){
+    return(
+        null
+    )
+}
+
+export function Attempts(){
+    return(
+        null
     )
 }
 
